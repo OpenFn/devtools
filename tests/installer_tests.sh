3ccc144d4ae3814b6968f4ed0dfab6b05cec327c
Taylor Downs <downs.taylor@gmail.com>
2017-09-04 15:36:04 -0300

    cleaner tests

--- tests/installer_tests.sh
+++ tests/installer_tests.sh
@@ -2,17 +2,20 @@
 
 # Demo sample_script.sh tests
 
-testInstallScript() {
-  # Load install script for testing
+testBaseInstallScript() {
+  echo "Test initial setup script..."
+
   . install.sh
 
-  echo "Test inistial setup script..."
   assertTrue 'Check default that core has been installed:' "[ -d core ] && [ -d core/node_modules ]"
   assertTrue 'Check default that language-common has been installed:' "[ -d language-common ] && [ -d language-common/node_modules ]"
+}
 
+testAdaptorInstallScript(){
   echo "Test language-package installation..."
 
   . install.sh language-salesforce
+
   assertTrue 'Check default that language-http has been installed:' "[ -d language-salesforce ] && [ -d language-salesforce/node_modules ]"
 }
 
